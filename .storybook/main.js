a72adce68a871cedcad5f3791db2197724683372
Artemelka <emelyanov.artem@wildberries.ru>
2020-12-17 05:57:48 +0300

    build(configure-package): init

---
+++ .storybook/main.js
@@ -0,0 +1,40 @@
+/* eslint-disable no-param-reassign */
+const path = require('path');
+const CompressionPlugin = require('compression-webpack-plugin');
+
+module.exports = {
+  stories: ['../lib/**/*.stories.mdx', '../lib/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-docs',
+    '@storybook/addon-essentials',
+    '@storybook/addon-knobs',
+    '@storybook/addon-viewport',
+  ],
+  webpackFinal: async (config, { configType }) => {
+    config.module.rules.push({
+      test: /(module)?\.s(a|c)ss$/,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+      include: path.resolve(__dirname, '../'),
+    });
+
+    if (configType === 'PRODUCTION') {
+      config.plugins.push(
+        new CompressionPlugin({
+          filename: '[path].gz[query]',
+          algorithm: 'gzip',
+          test: /\.js$|\.css$|\.json$|\.html$|\.ico$/,
+        }),
+      );
+    }
+
+    config.resolve = {
+      ...config.resolve,
+      alias: {
+        '@': path.resolve(__dirname, '../lib/'),
+      },
+    };
+
+    return config;
+  },
+};
